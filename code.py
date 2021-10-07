f09c7a780c6169765ba9f522449430ccc5a27ac7
Crestel Ong <90276376+crestel-ong@users.noreply.github.com>
2021-10-07 00:48:20 -0400

    lesson #2

--- code.py
+++ code.py
@@ -4,17 +4,31 @@
 # Created on: October 2021
 # This program is the "Space Aliens" program on the PyBadge
 
+import ugame
+import stage
 
 def game_scene():
-    # this function is the main game scene
+    # this function is the main game game_scene
 
-    print("\n\n\n")  # 3 blank lines
-    print("Hello, World!")
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
+    # set the background to image 0 in the image bank
+    #   and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render all the sprites
+    #  most likely you will only render the background once per game game scene
+    game.render_block()
+
+    # repeat forever, game Loop
     while True:
-        # repeat forever, or you turn it off!
-        pass  # just a place holder
-
+        pass # just a placeholder for now
 
 if __name__ == "__main__":
     game_scene()
