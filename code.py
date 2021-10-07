6d196946fd05659f8df45ed91cb24db13ab530a4
Crestel Ong <90276376+crestel-ong@users.noreply.github.com>
2021-10-06 22:34:19 -0400

    lesson #1

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+# Created by: Crestel Ong
+# Created on: October 2021
+# This program is the "Space Aliens" program on the PyBadge
+
+
+def game_scene():
+    # this function is the main game scene
+    
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World!")
+    
+    while True:
+        # repeat forever, or you turn it off!
+        pass # just a place holder
+
+
+if __name__ == "__main__":
+    game_scene()
